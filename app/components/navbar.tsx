e5d2868861894a5adbd5dbd69734855161b618f0
Hafsa-00 <159915448+Hafsa-00@users.noreply.github.com>
2024-12-07 22:00:06 +0500

    adding navbar

---
+++ app/components/navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+       <section className='py-3 bg-[#1d294f] text-white text-center px-10'>
+        <p className='text-sm'>Summer Sale: Save up to 40% on select items. Limited-time offer!</p>
+      </section>
+    </div>
+  )
+}
