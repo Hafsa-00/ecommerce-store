8308fdf4c70dff391d3e2adfacc5b6cf772366da
Hafsa-00 <159915448+Hafsa-00@users.noreply.github.com>
2024-12-07 22:41:28 +0500

    adding

---
+++ app/components/herosection.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Sidebar from './sidebar'
+
+export default function Herosection() {
+  return (
+    <div>
+      <Sidebar/>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@ import Image from "next/image";
 import Navbar from "./components/navbar";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import Herosection from "./components/herosection";
+import Productslist from "./components/productslist";
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
       <Header/>
+    <Herosection/>
+    <Productslist/>
       <Footer/>
     </div>
   );
